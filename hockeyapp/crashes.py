c7a3ec6e2c02307368db35225d926930fda759e2
Ali Lalani <alalani@marketcircle.com>
2013-07-25 15:26:21 -0400

    Added ability to provide a version_id, sort, order and search query for crashes.

--- hockeyapp/crashes.py
+++ hockeyapp/crashes.py
@@ -12,7 +12,7 @@ from . import api
 class CrashList(api.APIRequest):
     """This API lets you receive the list of crashes for an app."""
 
-    def __init__(self, api_key, app_id, page=1):
+    def __init__(self, api_key, app_id, version_id=None, sort=None, order=None, query=None, page=1):
         """Create the CrashLog request object.
 
         :param api_key: HockeyApp API key
@@ -27,6 +27,10 @@ class CrashList(api.APIRequest):
         self._key = None
         self._app_id = app_id
         self._page = page
+        self._version_id = version_id
+        self._sort = sort
+        self._order = order
+        self._query = query
 
     @property
     def parameters(self):
@@ -35,9 +39,17 @@ class CrashList(api.APIRequest):
         :returns: dict
 
         """
-        return {'symbolicated': 1,
-                'page': self._page}
+        dict = {'symbolicated': 1, 'page': self._page}
+        
+        if self._sort:
+            dict['sort'] = self._sort
+        if self._order:
+            dict['order'] = self._order
+        if self._query:
+            dict['query'] = self._query
 
+        return dict
+        
     @property
     def path(self):
         """Returns the request path
@@ -46,4 +58,15 @@ class CrashList(api.APIRequest):
 
         """
         params = urllib.urlencode(self.parameters)
-        return api.BASE_URI + 'apps/%s/crashes?%s' % (self._app_id, params)
+        
+        str = api.BASE_URI + 'apps/%s/' % (self._app_id)
+        
+        if self._version_id:
+            str = str + 'app_versions/%s/' % (self._version_id)
+        
+        str = str + 'crashes'
+        
+        if self._query:
+            str = str + '/search'
+        
+        return str
